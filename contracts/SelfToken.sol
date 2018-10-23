4e6e06020b4b82469f5e0083d9883402b1b5b203
Roger-Wu <gsetcrw@gmail.com>
2018-10-23 13:30:05 +0800

    change inheritance order

--- contracts/SelfToken.sol
+++ contracts/SelfToken.sol
@@ -4,13 +4,13 @@ import { ERC777ERC20BaseToken } from "./ERC777/ERC777ERC20BaseToken.sol";
 import { PausableERC777ERC20Token } from "./PausableERC777ERC20Token.sol";
 import { ERC777ERC20TokenWithOfficialOperators } from "./ERC777ERC20TokenWithOfficialOperators.sol";
 import { ERC777ERC20TokenWithApproveAndCall } from "./ERC777ERC20TokenWithApproveAndCall.sol";
-import { CappedMintableERC777ERC20Token } from "./CappedMintableERC777ERC20Token.sol";
 import { ERC777ERC20TokenWithBatchTransfer } from "./ERC777ERC20TokenWithBatchTransfer.sol";
+import { CappedMintableERC777ERC20Token } from "./CappedMintableERC777ERC20Token.sol";
 import { Claimable } from "openzeppelin-solidity/contracts/ownership/Claimable.sol";
 
 
 /// @dev The inheritance order is important.
-contract SelfToken is ERC777ERC20BaseToken, PausableERC777ERC20Token, ERC777ERC20TokenWithOfficialOperators, ERC777ERC20TokenWithApproveAndCall, CappedMintableERC777ERC20Token, ERC777ERC20TokenWithBatchTransfer, Claimable {
+contract SelfToken is ERC777ERC20BaseToken, PausableERC777ERC20Token, ERC777ERC20TokenWithOfficialOperators, ERC777ERC20TokenWithApproveAndCall, ERC777ERC20TokenWithBatchTransfer, CappedMintableERC777ERC20Token, Claimable {
   constructor()
     public
     ERC777ERC20BaseToken("SELF Token", "SELF", 1, new address[](0))
