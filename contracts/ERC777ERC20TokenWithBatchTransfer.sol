eeb3b9fe88e072a21e92f59ec71abb9f731a7860
Roger-Wu <gsetcrw@gmail.com>
2018-10-23 13:23:48 +0800

    add batchSend. add whenNotPaused

--- contracts/ERC777ERC20TokenWithBatchTransfer.sol
+++ contracts/ERC777ERC20TokenWithBatchTransfer.sol
@@ -1,17 +1,37 @@
 pragma solidity ^0.4.24;
 
-import { ERC777ERC20BaseToken } from "./ERC777/ERC777ERC20BaseToken.sol";
+import { PausableERC777ERC20Token } from "./PausableERC777ERC20Token.sol";
 
-contract ERC777ERC20TokenWithBatchTransfer is ERC777ERC20BaseToken {
+contract ERC777ERC20TokenWithBatchTransfer is PausableERC777ERC20Token {
   /// @notice ERC20 backwards compatible batch transfer.
   /// @param _recipients The addresses of the recipients
   /// @param _amounts The numbers of tokens to be transferred
   /// @return `true`, if the transfer can't be done, it should fail.
-  function batchTransfer(address[] _recipients, uint256[] _amounts) public erc20 returns (bool success) {
-    require(_recipients.length == _amounts.length, "the lengths of _recipients and _amounts should be the same");
+  function batchTransfer(address[] _recipients, uint256[] _amounts)
+    public
+    erc20
+    whenNotPaused
+    returns (bool success)
+  {
+    require(_recipients.length == _amounts.length, "The lengths of _recipients and _amounts should be the same.");
+
     for (uint256 i = 0; i < _recipients.length; i++) {
       doSend(msg.sender, msg.sender, _recipients[i], _amounts[i], "", "", false);
     }
     return true;
+  }
+
+  /// @notice Send tokens to multiple recipients.
+  /// @param _recipients The addresses of the recipients
+  /// @param _amounts The numbers of tokens to be transferred
+  function batchSend(address[] _recipients, uint256[] _amounts, bytes _userData)
+    whenNotPaused
+    external
+  {
+    require(_recipients.length == _amounts.length, "The lengths of _recipients and _amounts should be the same.");
+
+    for (uint256 i = 0; i < _recipients.length; i++) {
+      doSend(msg.sender, msg.sender, _recipients[i], _amounts[i], _userData, "", true);
+    }
   }
 }
