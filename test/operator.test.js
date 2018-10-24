1170f4cd7addda1095b378ff26f550d5bbc798c8
Lee Ting Ting <tleeae@connect.ust.hk>
2018-10-24 13:44:06 +0800

    change naming

--- test/operator.test.js
+++ test/operator.test.js
@@ -1,7 +1,7 @@
 const assertRevert = require("./helper/assertRevert");
 const ether = require("./helper/ether");
 const expectThrow = require("./helper/expectThrow");
-const ERC777ERC20TokenWithOfficialOperators = artifacts.require("ERC777ERC20TokenWithOfficialOperators");
+const SelfToken = artifacts.require("SelfToken");
 
 const BigNumber = web3.BigNumber;
 
@@ -10,13 +10,13 @@ const should = require('chai')
     .use(require('chai-bignumber')(BigNumber))
     .should();
 
-let tokenWithOfficialOperators;
+let selfToken;
 
-contract('ERC777ERC20TokenWithOfficialOperators', function (accounts) {
+contract('SelfToken', function (accounts) {
     // const [owner, buyer1, buyer2, buyer3, anyone] = accounts;
 
     it("should deploy new contract", async function () {
-        tokenWithOfficialOperators = await ERC777ERC20TokenWithOfficialOperators.new();
-        console.log(tokenWithOfficialOperators)
+        selfToken = await SelfToken.new();
+        console.log(selfToken)
     });
 });
