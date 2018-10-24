e06e034eab8333b9c88d9a6562d8c8580a5ae965
Lee Ting Ting <tleeae@connect.ust.hk>
2018-10-24 12:41:47 +0800

    add test

---
+++ test/operator.test.js
@@ -0,0 +1,21 @@
+const assertRevert = require("./helper/assertRevert");
+const ether = require("./helper/ether");
+const expectThrow = require("./helper/expectThrow");
+const ERC777ERC20TokenWithOfficialOperators = artifacts.require("ERC777ERC20TokenWithOfficialOperators");
+
+const BigNumber = web3.BigNumber;
+
+const should = require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
+
+let tokenWithOfficialOperators;
+
+contract('ERC777ERC20TokenWithOfficialOperators', function (accounts) {
+    const [owner, buyer1, buyer2, buyer3, anyone] = accounts;
+
+    it("should deploy new contract", async function () {
+        tokenWithOfficialOperators = await ERC777ERC20TokenWithOfficialOperators.new();
+    });
+});
