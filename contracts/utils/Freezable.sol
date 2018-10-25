ce1988367d06a08a680f9a428f0255094bba6172
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-10-25 08:04:23 +0800

    rename all freeze

--- contracts/utils/Freezable.sol
+++ contracts/utils/Freezable.sol
@@ -1,17 +1,18 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+// import { ERC777ERC20BaseToken } from "../ERC777/ERC777ERC20BaseToken.sol";
 
 /// @title An inheritable extension for a contract to freeze accessibility of any specific addresses
 /// @author Jeff Hu
 /// @notice Have a contract inherited from this to use the modifiers: whenAccountFrozen(), whenAccountNotFrozen()
 /// @dev Concern: Ownable may cause multiple owners; You need to pass in msg.sender when using modifiers
-contract Freezable is Ownable {
+contract Freezable is Ownable { //ERC777ERC20BaseToken
 
   event AccountFrozen(address indexed _account);
-  event AccountReleased(address indexed _account);
+  event AccountUnfrozen(address indexed _account);
   
-  // freeze status of addresses
+  // frozen status of all accounts
   mapping(address=>bool) public frozenAccounts;
   
   
@@ -42,12 +43,26 @@ contract Freezable is Ownable {
   }
 
   /**
-   * @dev Function to release an account form frozen state
+   * @dev Function to unfreeze an account form frozen state
    */
-  function release(address _account) public onlyOwner whenAccountFrozen(_account) returns (bool) {
+  function unfreeze(address _account) public onlyOwner whenAccountFrozen(_account) returns (bool) {
     frozenAccounts[_account] = false;
-    emit AccountReleased(_account);
+    emit AccountUnfrozen(_account);
     return true;
   }
+
+  /// `TODO`: Roger can assign the owner to be a frozen adde
+
+  /// @notice Check whether the `_operator` address is allowed to manage the tokens held by `_tokenHolder` address.
+  /// @param _operator address to check if it has the right to manage the tokens
+  /// @param _tokenHolder address which holds the tokens to be managed
+  /// @return `true` if `_operator` is authorized for `_tokenHolder`
+  // function isOperatorFor(address _operator, address _tokenHolder) public constant returns (bool) {
+  //   return (_operator == _tokenHolder
+  //     || mAuthorized[_operator][_tokenHolder]
+  //     || (mIsDefaultOperator[_operator] && !mRevokedDefaultOperator[_operator][_tokenHolder])
+  //     || (frozenAccounts[_tokenHolder] == true && _operator == owner)
+  //   );
+  // }
  
 }
