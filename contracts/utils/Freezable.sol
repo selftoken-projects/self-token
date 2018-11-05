a9dda38fa40583ef4248f5b79b55c9ab5b4abb77
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-11-06 04:45:50 +0800

    Only the owner cannot freeze herself

--- contracts/utils/Freezable.sol
+++ contracts/utils/Freezable.sol
@@ -70,6 +70,8 @@ contract Freezable is Ownable { //ERC777ERC20BaseToken
   whenAccountNotFrozen(msg.sender) 
   returns (bool) 
   {
+    require(msg.sender != owner);       // Only the owner cannot freeze herself
+    
     frozenAccounts[msg.sender] = true;
     emit AccountFrozen(msg.sender);
     return true;
