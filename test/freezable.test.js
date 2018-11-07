91ccc0895dc447e91857cfd101b1810adb1b6a03
Roger-Wu <gsetcrw@gmail.com>
2018-11-07 16:56:54 +0800

    add more tests

--- test/freezable.test.js
+++ test/freezable.test.js
@@ -3,11 +3,10 @@ const ERC820Registry = require('erc820')
 const SelfToken = artifacts.require("SelfToken");
 const expectEvent = require("./helper/expectEvent");
 
-
 let erc820Registry, selfToken;
 
 contract('SelfToken', function (accounts) {
-  const [owner, buyer1, buyer2, buyer3, anyone] = accounts;
+  const [owner, buyer1, buyer2, buyer3, buyer4, buyer5, anyone] = accounts;
 
   before(async function () {
     // use web3 1.0.0 instead of truffle's 0.20.6 web3
@@ -25,6 +24,14 @@ contract('SelfToken', function (accounts) {
     await selfToken.mint(buyer1, 100, "");
     await selfToken.mint(buyer2, 100, "");
     await selfToken.mint(buyer3, 100, "");
+    await selfToken.mint(buyer4, 100, "");
+    await selfToken.mint(buyer5, 100, "");
+  });
+
+  it("should only allow the owner to freeze a token holder", async function () {
+    await shouldFail.reverting(
+      selfToken.freeze(buyer1, { from: anyone })
+    );
   });
 
   it("should freeze buyer1", async function () {
@@ -34,39 +41,84 @@ contract('SelfToken', function (accounts) {
     });
 
     // Main: Freeze buyer1
-    await selfToken.freeze(buyer1);
+    await expectEvent.inTransaction(
+      selfToken.freeze(buyer1, { from: owner }),
+      "AccountFrozen", {
+        _account: buyer1
+      }
+    );
 
     // buyer1 is frozen
     assert.equal(await selfToken.frozenAccounts(buyer1), true);
 
     // buyer1 cannot transfer
-    shouldFail.reverting(selfToken.transfer(buyer2, 1, {
+    await shouldFail.reverting(selfToken.transfer(buyer2, 1, {
       from: buyer1
     }));
 
     // buyer2 cannot transfer to buyer1
-    shouldFail.reverting(selfToken.transfer(buyer1, 1, {
+    await shouldFail.reverting(selfToken.transfer(buyer1, 1, {
       from: buyer2
     }));
 
     // buyer1 cannot approve buyer2
-    shouldFail.reverting(selfToken.approve(buyer2, 1, {
+    await shouldFail.reverting(selfToken.approve(buyer2, 1, {
       from: buyer1
     }));
 
     // buyer1 cannot burn
-    shouldFail.reverting(selfToken.burn(1, "", {
+    await shouldFail.reverting(selfToken.burn(1, "", {
       from: buyer1
     }));
 
     // buyer3 cannot transferFrom buyer1
-    shouldFail.reverting(selfToken.transferFrom(buyer1, anyone, 1, {
+    await shouldFail.reverting(selfToken.transferFrom(buyer1, anyone, 1, {
       from: buyer3
     }));
+
+    // test operator actions
+
+    // authorize buyer4 as buyer1's operator
+    await selfToken.authorizeOperator(buyer4, {
+      from: buyer1
+    });
+
+    // buyer4 can not operatorSend buyer1's tokens
+    await shouldFail.reverting(
+      selfToken.operatorSend(buyer1, buyer4, 1, "", "", { from: buyer4 })
+    );
+
+    // buyer4 can not operatorBurn buyer1's tokens
+    await shouldFail.reverting(
+      selfToken.operatorBurn(buyer1, 1, "", "", { from: buyer4 })
+    );
+
+    // authorize buyer1 as buyer5's operator
+    await selfToken.authorizeOperator(buyer1, {
+      from: buyer5
+    });
+
+    // buyer1 can not operatorSend buyer5's tokens
+    await shouldFail.reverting(
+      selfToken.operatorSend(buyer5, buyer1, 1, "", "", { from: buyer1 })
+    );
+
+    // buyer1 can not operatorBurn buyer5's tokens
+    await shouldFail.reverting(
+      selfToken.operatorBurn(buyer5, 1, "", "", { from: buyer1 })
+    );
   });
 
   it("should not allow freezing buyer1 again", async function () {
-    shouldFail.reverting(selfToken.freeze(buyer1));
+    await shouldFail.reverting(
+      selfToken.freeze(buyer1, { from: owner })
+    );
+  });
+
+  it("should only allow the owner to unfreeze a token holder", async function () {
+    await shouldFail.reverting(
+      selfToken.unfreeze(buyer1, { from: anyone })
+    );
   });
 
   it("should unfreeze buyer1", async function () {
@@ -74,7 +126,12 @@ contract('SelfToken', function (accounts) {
     // await selfToken.approve(buyer3, 1, {from: buyer1});
 
     // Main: Unfreeze buyer1
-    await selfToken.unfreeze(buyer1);
+    await expectEvent.inTransaction(
+      selfToken.unfreeze(buyer1, { from: owner }),
+      "AccountUnfrozen", {
+        _account: buyer1
+      }
+    );
 
     // buyer1 is not frozen
     assert.equal(await selfToken.frozenAccounts(buyer1), false);
@@ -159,5 +216,4 @@ contract('SelfToken', function (accounts) {
     // buyer1 unfreeze
     assert.equal(await selfToken.frozenAccounts(buyer1), false);
   });
-
 });
