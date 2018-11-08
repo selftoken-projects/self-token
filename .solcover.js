9dbb4e071f3660fee39d655815eaa22f03e3e3f6
Lee Ting Ting <tleeae@connect.ust.hk>
2018-11-08 21:03:17 +0800

    ignore ERC820Client coverage

--- .solcover.js
+++ .solcover.js
@@ -6,5 +6,5 @@ module.exports = {
   // copyPackages: ['zeppelin-solidity'],
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile',
-  skipFiles: ['operators/BatchSendOperator.sol', 'utils/ExampleApprovalRecipient.sol', 'ERC777/examples']
+  skipFiles: ['operators/BatchSendOperator.sol', 'utils/ExampleApprovalRecipient.sol', 'ERC777/examples', 'ERC820/ERC820Client']
 };
