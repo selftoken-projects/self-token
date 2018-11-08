4f42389429f4f9470ef4f9fac75a91a2f33cbc7d
Lee Ting Ting <tleeae@connect.ust.hk>
2018-11-08 11:17:22 +0800

    add comments

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8555,
   // testrpcOptions: '-p 8555',
-  norpc: true,
+  norpc: true, // true if we want to launch our own testrpc instance 
   // dir: './secretDirectory',
   // copyPackages: ['zeppelin-solidity'],
   // skipFiles: ['Routers/EtherRouter.sol']
