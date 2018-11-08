d38741d29d4601c7a39adad406f47a045d52b76b
Guan-Rong Wu <Roger-Wu@users.noreply.github.com>
2018-11-09 01:28:10 +0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -19,15 +19,15 @@ module.exports = {
     optimizer: {
       enabled: true,
       runs: 200
-    },
-    networks: {
-      coverage: {
-        host: "localhost",
-        network_id: "*",
-        port: 8555, // <-- If you change this, also set the port option in .solcover.js.
-        gas: 0xfffffffffff, // <-- Use this high gas value
-        gasPrice: 0x01 // <-- Use this low gas price
-      }
+    }
+  },
+  networks: {
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01 // <-- Use this low gas price
     }
   }
 };
