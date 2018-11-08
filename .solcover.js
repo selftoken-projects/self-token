1189fc84b8abd94a4b29f560b68b5a9f9715b9de
Lee Ting Ting <tleeae@connect.ust.hk>
2018-11-08 11:16:43 +0800

    configure solidity coverage settings

---
+++ .solcover.js
@@ -0,0 +1,8 @@
+module.exports = {
+  port: 8555,
+  // testrpcOptions: '-p 8555',
+  norpc: true,
+  // dir: './secretDirectory',
+  // copyPackages: ['zeppelin-solidity'],
+  // skipFiles: ['Routers/EtherRouter.sol']
+};
