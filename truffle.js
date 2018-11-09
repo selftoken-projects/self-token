7e22e7b1cdc30cd984720c33a989636c6034c522
Lee Ting Ting <tleeae@connect.ust.hk>
2018-11-09 12:01:08 +0800

    fix truffle migration settings

--- truffle.js
+++ truffle.js
@@ -19,15 +19,20 @@ module.exports = {
     optimizer: {
       enabled: true,
       runs: 200
+    }
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
     },
-    networks: {
-      coverage: {
-        host: "localhost",
-        network_id: "*",
-        port: 8555, // <-- If you change this, also set the port option in .solcover.js.
-        gas: 0xfffffffffff, // <-- Use this high gas value
-        gasPrice: 0x01 // <-- Use this low gas price
-      }
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555, // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01 // <-- Use this low gas price
     }
   }
 };
